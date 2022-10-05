ef4cb42c350c7dd8d3df3a7e4e2bcfd2573501a0
Farhad-git-vm <f.golshann@gmail.com>
2022-10-05 07:50:58 +0330

    myfavoritecolor

---
+++ slicinglist.py
@@ -0,0 +1,9 @@
+colors = ["red","blue","brown"]
+print(colors)
+colors.reverse()
+print(colors)
+
+newcolors = ["yellow","green","white","black"]
+print(newcolors)
+copy_of_new_color = newcolors[::-1]
+print(copy_of_new_color)
