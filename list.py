478a1fd4b4e94a658eb9299e13f7ec1867b74c08
Farhad-git-vm <f.golshann@gmail.com>
2022-10-05 13:10:14 +0330

    for testing merge

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
 myNumber = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 selectmynumber = [3]
 print(selectmynumber)
+snfl = myNumber[1:11:2]
+print(snfl)
--- slicinglist.py
+++ slicinglist.py
@@ -7,3 +7,5 @@ newcolors = ["yellow","green","white","black"]
 print(newcolors)
 copy_of_new_color = newcolors[::-1]
 print(copy_of_new_color)
+
+#This is only for training
